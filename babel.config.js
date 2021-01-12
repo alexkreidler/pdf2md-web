ecde40fdd94ef0de67e98be93a3e064bdc915dc7
Alex Kreidler <alexkreidler2020@gmail.com>
2021-01-12 12:56:27 -0500

    update versions and webpack config, format

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,20 @@
-
-module.exports = api => {
-  api.cache(true)
+module.exports = (api) => {
+  api.cache(true);
   return {
-    "plugins": [
-      "@babel/plugin-proposal-class-properties"
-    ],
-    "presets": [
-      [ "@babel/preset-env", { "loose": false, "modules": false, "targets": "> 0.25%, not dead"} ],
+    plugins: ["@babel/plugin-proposal-class-properties"],
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          // useBuiltIns: "usage",
+          // corejs: 3,
+          // loose: false,
+          // modules: false,
+          // targets: "> 0.25%, not dead",
+        },
+      ],
       "@babel/preset-react",
-      "@babel/preset-flow"
+      "@babel/preset-flow",
     ],
-  }
-}
+  };
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,97 +1,129 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
 
-var SOURCE_DIR = path.resolve(__dirname, 'src');
-var JAVASCRIPT_DIR = SOURCE_DIR + '/javascript';
-var BUILD_DIR = path.resolve(__dirname, 'build');
-var NODEMODULES_DIR = path.resolve(__dirname, 'node_modules');
+var SOURCE_DIR = path.resolve(__dirname, "src");
+var JAVASCRIPT_DIR = SOURCE_DIR + "/javascript";
+var BUILD_DIR = path.resolve(__dirname, "build");
+var NODEMODULES_DIR = path.resolve(__dirname, "node_modules");
 
-module.exports = {
-    context: SOURCE_DIR,
-    resolve: {
-        modules: [
-            path.resolve(JAVASCRIPT_DIR),
-            path.resolve(NODEMODULES_DIR + '/@opendocsg/pdf2md/lib'),
-            path.resolve(NODEMODULES_DIR)
-        ]
-    },
-    entry: {
-        app: ['core-js', 'regenerator-runtime/runtime', './javascript/index.jsx']
-    },
-    output: {
-        path: BUILD_DIR,
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                // Ask webpack to check: If this file ends with .js, then apply some transforms
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                options: {
-                    configFile: path.resolve('babel.config.js')
-                },
-                include: [JAVASCRIPT_DIR, NODEMODULES_DIR + '/@opendocsg/pdf2md/lib'],
-            },
-            {
-                test: /\.css$/,
-                loader: "style-loader!css-loader"
-            },
-            {
-                test: /\.png$/,
-                loader: "url-loader?limit=100000"
-            },
-            {
-                test: /\.jpg$/,
-                loader: "file-loader"
-            },
-            {
-                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'file-loader'
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html'
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'version': JSON.stringify(process.env.npm_package_version),
-                NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
-            }
-        }),
-        new CopyWebpackPlugin([
-            {
-                from: NODEMODULES_DIR + '/pdfjs-dist/build/pdf.worker.js',
-                to: 'bundle.worker.js'
-            },
-        ]),
-        new CopyWebpackPlugin([
-            {
-                from: NODEMODULES_DIR + '/pdfjs-dist/cmaps',
-                to: 'cmaps'
-            },
-        ]),
-        new CopyWebpackPlugin([
-            {
-                from: 'favicons',
-                to: 'favicons'
-            },
-        ])
-    ]
-}
+/** @type import('webpack').Configuration */
+var config = {
+  context: SOURCE_DIR,
+  resolve: {
+    modules: [
+      path.resolve(JAVASCRIPT_DIR),
+      path.resolve(NODEMODULES_DIR + "/@opendocsg/pdf2md/lib"),
+      path.resolve(NODEMODULES_DIR),
+    ],
+  },
+  entry: {
+    app: ["core-js", "regenerator-runtime/runtime", "./javascript/index.jsx"],
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        // Ask webpack to check: If this file ends with .js, then apply some transforms
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          configFile: path.resolve("babel.config.js"),
+        },
+        include: [JAVASCRIPT_DIR, NODEMODULES_DIR + "/@opendocsg/pdf2md/lib"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.png$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 100000,
+          },
+        },
+      },
+      {
+        test: /\.jpg$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 100000,
+            mimetype: "application/font-woff",
+          },
+        },
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 100000,
+            mimetype: "application/octet-stream",
+          },
+        },
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 100000,
+            mimetype: "image/svg+xml",
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        version: JSON.stringify(process.env.npm_package_version),
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || "development"),
+      },
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: NODEMODULES_DIR + "/pdfjs-dist/build/pdf.worker.js",
+          to: "bundle.worker.js",
+        },
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: NODEMODULES_DIR + "/pdfjs-dist/cmaps",
+          to: "cmaps",
+        },
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "favicons",
+          to: "favicons",
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = config;
